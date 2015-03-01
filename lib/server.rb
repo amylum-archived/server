ba5457914f0490bed9222e1886ae9acae3c38aa2
Les Aker <me@lesaker.org>
2015-02-28 21:25:56 -0500

    clean up cops

--- lib/server.rb
+++ lib/server.rb
@@ -1,22 +1,26 @@
 require 'sinatra/base'
 require 's3repo'
 
+##
+# Server module, defines constants and main Sinatra class
 module Server
   REPO_NAME = ENV['S3REPO_NAME'] || ENV['S3_BUCKET']
   fail('Please set S3REPO_NAME or S3_BUCKET') unless REPO_NAME
 
   REPO = S3Repo.new
 
+  ##
+  # Main Sinatra class, routes incoming requests
   class Base < Sinatra::Base
     before do
       content_type 'application/octet-stream'
     end
 
-    get %r{^/#{REPO_NAME}\.db(?:\.tar\.gz)?$} do
+    get(/^\/#{REPO_NAME}\.db(?:\.tar\.gz)?$/) do
       REPO.serve 'repo.db'
     end
 
-    get %r{^/([\w-]+)\.pkg\.tar\.xz$} do |package|
+    get(/^\/([\w-]+)\.pkg\.tar\.xz$/) do |package|
       REPO.serve "#{package}.pkg.tar.xz"
     end
   end
