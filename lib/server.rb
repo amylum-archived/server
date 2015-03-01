fa6ed5f4c5b82442e41165d21a0118b04d8307ea
Les Aker <me@lesaker.org>
2015-02-28 21:14:16 -0500

    rename repo

--- lib/server.rb
+++ lib/server.rb
@@ -5,17 +5,17 @@ module Server
   REPO_NAME = ENV['S3REPO_NAME'] || ENV['S3_BUCKET']
   fail('Please set S3REPO_NAME or S3_BUCKET') unless REPO_NAME
 
-  CLIENT = S3Repo.new
+  REPO = S3Repo.new
 
   class Base < Sinatra::Base
     enable :logging
 
     get %r{^/#{REPO_NAME}\.db(?:\.tar\.gz)?$} do
-      CLIENT.serve("#{REPO_NAME}.db")
+      REPO.serve 'repo.db'
     end
 
     get %r{^/([\w-]+)\.pkg\.tar\.xz$} do |package|
-      CLIENT.serve("#{package}.pkg.tar.xz", false)
+      REPO.serve "#{package}.pkg.tar.xz"
     end
   end
 end
