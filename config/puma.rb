18b76b4b1eca895f0e0af3e150178339df8fa761
Les Aker <me@lesaker.org>
2015-08-30 09:32:17 -0400

    clean up dual binding

--- config/puma.rb
+++ config/puma.rb
@@ -1,8 +1,16 @@
 address = ENV['ADDRESS'] || '[::]'
-port = ENV['PORT'] || 9292
+
+http_port = ENV['HTTP_PORT'] || ENV['PORT'] || 80
+bind "tcp://#{address}:#{http_port}"
 
 if ENV['SSL_DIR']
-  ssl_bind address, port, key: ENV['SSL_DIR'] + '/key', cert: ENV['SSL_DIR'] + '/cert'
+  https_port = ENV['HTTPS_PORT'] || 443
+  ssl_bind(
+    address,
+    https_port,
+    key: ENV['SSL_DIR'] + '/key',
+    cert: ENV['SSL_DIR'] + '/cert'
+  )
 else
   bind "tcp://#{address}:#{port}"
 end
