e9d46c81f472147038b5d758ad668f058ef66c3d
Les Aker <me@lesaker.org>
2015-08-30 11:25:36 -0400

    update with fixes

--- config/puma.rb
+++ config/puma.rb
@@ -1,14 +1,14 @@
-address = ENV['ADDRESS'] || 'localhost'
+address = ENV['PUMA_ADDRESS'] || 'localhost'
 
-http_port = ENV['HTTP_PORT'] || ENV['PORT'] || 8080
+http_port = ENV['PUMA_HTTP_PORT'] || ENV['PUMA_PORT'] || 8080
 bind "tcp://#{address}:#{http_port}"
 
 if ENV['PUMA_SSL']
-  https_port = ENV['HTTPS_PORT'] || 8443
+  https_port = ENV['PUMA_HTTPS_PORT'] || 8443
   ssl_bind(
     address,
     https_port,
-    key: ENV['SSL_DIR'] + '/key',
-    cert: ENV['SSL_DIR'] + '/cert'
+    key: ENV['PUMA_SSL_DIR'] + '/key',
+    cert: ENV['PUMA_SSL_DIR'] + '/cert'
   )
 end
